b3ebb1863794952342872e2d0463f8738b8b5d6f
valentinavega <valentinavega94@gmail.com>
2019-05-21 15:08:47 -0400

    Add play and pause functionality

---
+++ scripts/song-list.js
@@ -0,0 +1,17 @@
+{
+    album.songs.forEach( (song, index) => {
+      song.element = $(`
+        <tr>
+          <td>${index+1}</td>
+          <td>${song.title}</td>
+          <td>${song.duration}</td>
+         </tr>
+      `);
+  
+      song.element.on('click', event => {
+        player.playPause(song);
+      });
+      
+      $('#song-list').append(song.element);    
+    });
+  }
