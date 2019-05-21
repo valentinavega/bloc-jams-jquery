7021c7380f8691c24cd143d263b44a8f9830feee
valentinavega <valentinavega94@gmail.com>
2019-05-21 15:21:03 -0400

    Checkpoint-2592

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -2,7 +2,13 @@
     album.songs.forEach( (song, index) => {
       song.element = $(`
         <tr>
-          <td>${index+1}</td>
+          <td>
+          <button>
+            <span class="song-number">${index + 1}</span>
+            <span class="ion-play"></span>
+            <span class="ion-pause"></span>
+          </button>
+        </td>
           <td>${song.title}</td>
           <td>${song.duration}</td>
          </tr>
@@ -11,7 +17,7 @@
       song.element.on('click', event => {
         player.playPause(song);
       });
-      
+
       $('#song-list').append(song.element);    
     });
   }
