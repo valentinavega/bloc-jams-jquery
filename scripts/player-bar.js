75f2e1415d4ff9bf7a3c9fdab6b0d4d8c9ac1e78
valentinavega <valentinavega94@gmail.com>
2019-05-21 16:34:01 -0400

    Checkpoint 2593: Complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -16,4 +16,14 @@
     });
   }
 
+//Build previous button
 
+$('button#previous').on('click', function() {
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex -1;
+    if (previousSongIndex < 0 ) { return; }
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
+})
